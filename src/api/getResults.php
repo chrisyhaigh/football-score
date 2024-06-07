ec8117cbc75c60c9982c9a6ba981e0a5ca71de4d
Chrisy <chrisyhaigh@gmail.com>
2024-06-07 15:08:33 +0100

    Backend set up with correct parameters for league matchday results

---
+++ src/api/getResults.php
@@ -0,0 +1,82 @@
+<?php
+
+    ini_set('display_errors', 'on');
+    error_reporting(E_ALL);
+
+    $executionTime = microtime(true);
+
+    $season = $_GET['season'];
+    $leagueID = $_GET['id'];
+    $matchday = $_GET['matchday'];
+
+    if (!$season || !$leagueID || !$matchday) {
+        echo json_encode([
+            'status' => [
+                'code' => '400',
+                'name' => 'error',
+                'description' => 'Invalid Parameters',
+                'returnTime' => intval((microtime(true) - $executionTime))
+            ]
+            ]);
+        exit;
+    }
+
+    $apiKey = "01058054494548bba8346396848d37d8";
+
+    $url = 'https://api.football-data.org/v4/competitions/' . $leagueID . '/matches?season=' . $season . '&matchday=' . $matchday;
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'X-Auth-Token: ' . $apiKey
+    ]);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        $output = [
+            'status' => [
+                'code' => '500',
+                'name' => 'error',
+                'description' => 'CURL_ERROR: ' . curl_error($ch),
+                'returnTime' => intval((microtime(true) - $executionTime))
+            ]
+            ];
+    } else {
+        $decode = json_decode($response, true);
+
+        if ($decode === null) {
+            $output = [
+                'status' => [
+                    'code' => '500',
+                    'name' => 'error',
+                    'description' => 'JSON_DECODE_ERROR: ' . json_last_error_msg(),
+                    'returnTime' => intval((microtime(true) - $executionTime))
+                ]
+                ];
+        } else {
+            $output = [
+                'status' => [
+                    'code' => '200',
+                    'name' => 'success',
+                    'description' => 'ok',
+                    'returnTime' => intval((microtime(true) - $executionTime))
+                ],
+                'data' => $decode
+            ];
+        }
+    }
+
+    curl_close($ch);
+
+    header('Content-Type: application/json; charset=UTF-8');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    echo json_encode($output);
+
+?>
