f57b0cf9d2a55808820b8e1b99449c20f823b9cb
Chrisy <chrisyhaigh@gmail.com>
2024-07-12 08:03:49 +0100

    Dark/light mode function added to Navbar for brightness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,38 @@
 import React, { useState } from "react";
-import '../css/Navbar.css'
-import { NavLink, Link } from "react-router-dom";
+import '../css/Navbar.css';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [sideBarVisible, setSideBarVisible] = useState(false);
 
     const toggleSideBar = () => {
         setSideBarVisible(!sideBarVisible);
-    }
+    };
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
                 <Link to="/home">
                     <div className="logo-container">
-                        <img className="logo-image" src="src\images\image_processing20200410-18222-12ilqee.png" alt="Logo" />
+                        <img className="logo-image" src="src/images/image_processing20200410-18222-12ilqee.png" alt="Logo" />
                     </div>
                 </Link>
                 <div className="nav-name-container">
                     <p className="nav-name">GOAL FOOTBALL</p>
                 </div>
-                <button id="sidebar-toggle" className="navbar-toggler" type="button" onClick={toggleSideBar} aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className={`collapse navbar-collapse ${sideBarVisible ? 'show' : ''}`} id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <NavLink style={{textDecoration: 'none', color: 'black'}} to="/tables" className="nav-link active" aria-current="page">TABLES</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink style={{textDecoration: 'none', color: 'black'}} to="/scorers" className="nav-link">SCORERS</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink style={{textDecoration: 'none', color: 'black'}} to="/results" className="nav-link">RESULTS</NavLink>
-                        </li>
-                    </ul>
-                </div>
+                <form className="brightness-mode">
+                    <label className="theme-option">
+                        <i className="fa-solid fa-moon"></i>
+                        <input type="radio" name="theme" value="dark" />
+                    </label>
+                    <label className="theme-option">
+                        <i className="fa-solid fa-sun"></i>
+                        <input type="radio" name="theme" value="light" />
+                    </label>
+                </form>
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
