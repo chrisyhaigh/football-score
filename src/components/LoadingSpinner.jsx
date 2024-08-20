38b369c867f62d681671cf8f8af7dc62a6eed107
Chrisy <chrisyhaigh@gmail.com>
2024-08-20 16:35:34 +0100

    Preloader spinner component added for display when HTML is rendering

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+
+    return (
+        <div>
+            <div className="loader-container">
+                <div className="loader">Loading</div>
+            </div>
+        </div>
+    )
+
+}
+
+export default LoadingSpinner;
